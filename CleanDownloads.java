b1b3ab5cdd7db39953a6c0a5079ce04dcdfb1960
Sreehari Anukumar <146329468+Sreehari05055@users.noreply.github.com>
2024-10-11 12:27:45 +0100

    Adding File Organizer

---
+++ CleanDownloads.java
@@ -0,0 +1,118 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class CleanDownloads {
+    private String userFileName;
+    public String publicDir;
+    public String audioDir;
+    public String videoDir;
+    File Obj;
+     File [] files;
+
+    public CleanDownloads() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file holder name: ");
+        String holderName = scanner.nextLine();
+        HolderName(holderName.strip());
+        publicDir = "/Users/"+userFileName+"/Documents";
+        audioDir = "/Users/"+userFileName+"/Music";
+        videoDir = "/Users/"+userFileName+"/Pictures";
+        Obj = new File("/Users/"+userFileName+"/Downloads");
+
+        files = Obj.listFiles();
+        FurtherProcessing(files);
+        System.out.println("Process Completed");
+    }
+    public void FurtherProcessing(File[] obj){
+        if(obj != null){
+            for(File file: obj){
+                if(file.getName().toLowerCase().endsWith(".mp3") || file.getName().toLowerCase().endsWith(".m4a")){
+                    try{
+                        Path targetDirPath = Paths.get(audioDir);
+                        if (Files.notExists(targetDirPath)) {
+                            try {
+                                Files.createDirectory(targetDirPath);
+                                System.out.println("Directory created: " + targetDirPath);
+                        } catch (IOException e) {
+                                System.err.println("Error creating directory: " + e.getMessage());
+                            }
+                            if (Files.isDirectory(targetDirPath)) {
+                                try {
+                                    Files.move(file.toPath(), targetDirPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    System.err.println("Error moving file: " + e.getMessage());
+                                }
+                            }
+
+                        }
+                    else{
+                        Files.move(file.toPath(),targetDirPath.resolve(file.getName()),StandardCopyOption.REPLACE_EXISTING);
+                    }
+                    }
+                    catch (IOException E){
+                        System.err.println("Error: " + E.getMessage());
+                        E.printStackTrace();
+                    }
+            }else if(file.getName().toLowerCase().endsWith(".mp4") || file.getName().toLowerCase().endsWith(".jpeg") || file.getName().toLowerCase().endsWith(".jpg")){
+                    try{
+                        Path targetDirVPath = Paths.get(videoDir);
+                        if (Files.notExists(targetDirVPath)) {
+                            try {
+                                Files.createDirectory(targetDirVPath);
+                                System.out.println("Directory created: " + targetDirVPath);
+                            } catch (IOException e) {
+                                System.err.println("Error creating directory: " + e.getMessage());
+                            }
+                            if (Files.isDirectory(targetDirVPath)) {
+                                try {
+                                    Files.move(file.toPath(), targetDirVPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    System.err.println("Error moving file: " + e.getMessage());
+                                }
+                            }
+                        }
+                        else{
+                            Files.move(file.toPath(),targetDirVPath.resolve(file.getName()),StandardCopyOption.REPLACE_EXISTING);
+                    }}catch (IOException e){
+                        System.out.println("Error: "+e.getMessage());
+                        e.printStackTrace();
+                    }
+                }else if(file.getName().toLowerCase().endsWith(".zip") || file.getName().toLowerCase().endsWith(".doc") || file.getName().toLowerCase().endsWith(".docx") || file.getName().toLowerCase().endsWith(".txt") || file.getName().toLowerCase().endsWith(".pdf")){
+                    try{
+                        Path targetPDirPath = Paths.get(publicDir);
+                        if (Files.notExists(targetPDirPath)) {
+                            try {
+                                Files.createDirectory(targetPDirPath);
+                                System.out.println("Directory created: " + targetPDirPath);
+                            } catch (IOException e) {
+                                System.err.println("Error creating directory: " + e.getMessage());
+                            }
+                                if (Files.isDirectory(targetPDirPath)) {
+                                    try {
+                                        Files.move(file.toPath(), targetPDirPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                                    } catch (IOException e) {
+                                        System.err.println("Error moving file: " + e.getMessage());
+                                    }
+                                }
+                            }
+                        else{
+                            Files.move(file.toPath(),targetPDirPath.resolve(file.getName()),StandardCopyOption.REPLACE_EXISTING);
+                        }
+                    }
+                    catch (IOException e){
+                        System.out.println("Error: "+e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+    public void HolderName(String name){
+        this.userFileName = name;
+    }
+}
+
+
---
+++ CleanMusic.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class CleanMusic
+
+{   private String userName;
+    public String docDir;
+    public String audDir;
+    public String vidDir;
+    public String dldDir;
+    File musicObj;
+     File [] musicFiles;
+    public CleanMusic()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file holder name: ");
+        String holderName = scanner.nextLine();
+        Holder(holderName.strip());
+        dldDir = "/Users/"+userName+"/Downloads";
+        docDir = "/Users/"+userName+"/Documents";
+        audDir = "/Users/"+userName+"/Downloads";
+        vidDir = "/Users/"+userName+"/Pictures";
+
+        musicObj = new File("/Users/"+userName+"/Music");
+
+        musicFiles = musicObj.listFiles();
+        FurtherProcess(musicFiles);
+    }
+    public void Holder(String name){
+        this.userName = name;
+    }
+    public void FurtherProcess(File [] obj2)
+    {
+        if(obj2 != null)
+        {
+            for(File file: obj2)
+            {
+                if(file.getName().toLowerCase().endsWith(".mp4") || file.getName().toLowerCase().endsWith(".jpeg") || file.getName().toLowerCase().endsWith(".jpg")){
+                    try{
+                        Path targetDirVPath = Paths.get(vidDir);
+                        if (Files.notExists(targetDirVPath)) {
+                            try {
+                                Files.createDirectory(targetDirVPath);
+                                System.out.println("Directory created: " + targetDirVPath);
+                            } catch (IOException e) {
+                                System.err.println("Error creating directory: " + e.getMessage());
+                            }
+                            if (Files.isDirectory(targetDirVPath)) {
+                                try {
+                                    Files.move(file.toPath(), targetDirVPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    System.err.println("Error moving file: " + e.getMessage());
+                                }
+                            }
+                        }
+                        else{
+                            Files.move(file.toPath(),targetDirVPath.resolve(file.getName()),StandardCopyOption.REPLACE_EXISTING);
+                        }}catch (IOException e){
+                        System.out.println("Error: "+e.getMessage());
+                        e.printStackTrace();
+                    }
+
+        }else if(file.getName().toLowerCase().endsWith(".zip") || file.getName().toLowerCase().endsWith(".doc") || file.getName().toLowerCase().endsWith(".docx") || file.getName().toLowerCase().endsWith(".txt") || file.getName().toLowerCase().endsWith(".pdf")){
+                    try{
+                        Path targetPDirPath = Paths.get(docDir);
+                        if (Files.notExists(targetPDirPath)) {
+                            try {
+                                Files.createDirectory(targetPDirPath);
+                                System.out.println("Directory created: " + targetPDirPath);
+                            } catch (IOException e) {
+                                System.err.println("Error creating directory: " + e.getMessage());
+                            }
+                            if (Files.isDirectory(targetPDirPath)) {
+                                try {
+                                    Files.move(file.toPath(), targetPDirPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    System.err.println("Error moving file: " + e.getMessage());
+                                }
+                            }
+                        }
+                        else{
+                            Files.move(file.toPath(),targetPDirPath.resolve(file.getName()),StandardCopyOption.REPLACE_EXISTING);
+                        }}catch (IOException e){
+                        System.out.println("Error: "+e.getMessage());
+                        e.printStackTrace();
+                    }
+
+        }
+    }
+}
+    }
+}
